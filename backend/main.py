df6040e095ea9f7f3292a0bb8e3b2edd63340623
willievuong <willievuong@gmail.com>
2022-11-08 15:44:36 -0800

    Docker boilerplate

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return {'msg': 'From one to America, how free are you tonight? Henry ;)'}, 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0",port=6000)
