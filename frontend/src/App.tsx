99c1a0e3cbbb1f1792e93a7360bb3b5ae589bd74
willievuong <willievuong@gmail.com>
2022-12-08 20:13:57 -0800

    Added icons package and Auth navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
         <Navbar/>
       </header>
       <body>
