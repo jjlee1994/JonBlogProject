01c4c928d987e0879667d522b5828ba84f9b357f
jjlee1994 <jonathanjlee1994@gmail.com>
2023-07-12 21:35:58 -0700

    adding missing modified files from prev commit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,25 @@
 import { Grid } from "@mui/material"
 import PostCard from "../components/PostCard"
 import UserPostEntryCard from "../components/UserPostEntryCard"
+import axios from 'axios'
 
 function Home(){
+
+    async function test(){
+        const response = await axios({
+            method: 'post',
+            url: 'http://localhost:5001/post',
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4OTIyMTExMywianRpIjoiMjhjYmRlOTYtZTRlYi00YWUzLWI1NDMtMmNkZjM4OGUxNmJlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IkgzIiwibmJmIjoxNjg5MjIxMTEzLCJleHAiOjE2ODkyMjIwMTN9.aMCjQsxzYLADJS2iYCWuOf0V-IuCckX2NVilD3_hgwc'
+            },
+            data:{
+                'post_id': '1'
+            }
+        });
+        console.log(response.data);
+    }
+
+    test()
 
     return (
         <div>
